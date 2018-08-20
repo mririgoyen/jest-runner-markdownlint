07f526dcdee85f73746549849842bf01f20e0e37
Michael Irigoyen <mirigoyen@accusoft.com>
2018-08-20 11:07:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+<div align="center">
+  <img width="150" src="https://cdn.worldvectorlogo.com/logos/markdown.svg">
+  <a href="https://facebook.github.io/jest/">
+    <img height="100" vspace="" hspace="25" src="https://cdn.worldvectorlogo.com/logos/jest.svg">
+  </a>
+  <h1>jest-runner-markdownlint</h1>
+  <p>Markdown Lint runner for Jest</p>
+</div>
+
+# Usage
+
+## Install
+
+Install `jest` _(Jest 21+)_ and `jest-runner-markdownlint`
+
+```bash
+yarn add --dev jest jest-runner-markdownlint
+```
+
+### Add it to your Jest config
+
+In your `package.json`
+
+```json
+{
+  "jest": {
+    "runner": "jest-runner-markdownlint",
+    "displayName": "Markdown Lint",
+    "testMatch": ["<rootDir>/src/**/*.md"],
+    "moduleFileExtensions": ["md"]
+  }
+}
+```
+
+Or in `jest.config.js`
+
+```js
+module.exports = {
+  runner: 'jest-runner-markdownlint',
+  displayName: 'Markdown Lint',
+  testMatch: ['<rootDir>/src/**/*.md'],
+  moduleFileExtensions: ['md']
+}
+```
+
+### Run Jest
+
+```bash
+yarn jest
+```
---
+++ src/run.js
@@ -0,0 +1,77 @@
+const { fail, pass } = require('create-jest-runner');
+const lint = require('markdownlint');
+const chalk = require('chalk');
+const table = require('text-table');
+const stripAnsi = require('strip-ansi');
+const fs = require('fs');
+const path = require('path');
+
+const DEFAULT_CONFIG_FILE = '.markdownlintrc';
+
+module.exports = ({ config, testPath }) => {
+  const start = Date.now();
+  const { displayName, rootDir } = config;
+  const options = {
+    files: [ testPath ]
+  };
+
+  if (fs.existsSync(`${rootDir}/${DEFAULT_CONFIG_FILE}`)) {
+    options.config = require(`${rootDir}/${DEFAULT_CONFIG_FILE}`);
+  }
+
+  const results = lint.sync(options);
+
+  if (!(testPath in results)) {
+    return fail({
+      start,
+      end: Date.now(),
+      test: {
+        errorMessage: 'Unable to lint file',
+        path: testPath,
+        title: displayName
+      }
+    });
+  }
+
+  const result = results[testPath];
+
+  if (result.length > 0) {
+    let output = '\n';
+    output += `${chalk.underline(testPath)}\n`;
+
+    output += `${table(
+      result.map(message => {
+        return [
+          '',
+          Array.isArray(message.errorRange) ? message.errorRange[0] : 0,
+          Array.isArray(message.errorRange) ? message.errorRange[1] : 0,
+          chalk.red('error'),
+          message.ruleDescription || '',
+          chalk.dim(Array.isArray(message.ruleNames) ? message.ruleNames[1] : '')
+        ];
+      }),
+      {
+        align: ['', 'r', 'l'],
+        stringLength (str) {
+          return stripAnsi(str).length;
+        }
+      }
+    ).split('\n').map(el => el.replace(/(\d+)\s+(\d+)/, (m, p1, p2) => chalk.dim(`${p1}:${p2}`))).join('\n')}\n\n`;
+
+    return fail({
+      start,
+      end: Date.now(),
+      test: {
+        errorMessage: output,
+        path: testPath,
+        title: displayName
+      }
+    });
+  }
+
+  return pass({
+    start,
+    end: Date.now(),
+    test: { path: testPath, title: displayName }
+  });
+};
